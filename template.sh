8cec76a8aec1c18fdf38fc42992b8c1467db01f9
Satyajit Roy <rsatyajit777@gmail.com>
2025-10-07 09:25:28 +0600

    update folder structure

---
+++ template.sh
@@ -0,0 +1,21 @@
+
+#  here i am creatind directories
+mkdir -p src
+mkdir -p research
+
+#  here i creating file
+
+touch src/__init__.py #constructor file
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+
+echo " file directory created successfully!"
+
+
